8d03132596c0593fa03f196159f74ad4d84b415e
Семён Бабушкин <babushkin.semyon@gmail.com>
2022-11-09 12:00:28 +0800

    new hub repo initial commit

---
+++ src/application/typings/routes.ts
@@ -0,0 +1,11 @@
+export enum RoutesEnum {
+  send = '/send',
+  myAssets = '/my-assets',
+  discover = '/discover',
+  myPlace = '/my-place',
+  build = '/build',
+  contribute = '/contribute',
+  signup = '/signup',
+  root = '/',
+  login = '/login',
+}
