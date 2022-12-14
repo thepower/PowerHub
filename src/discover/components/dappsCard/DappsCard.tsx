a074b1ef27a12b17afac79d91bf702d21908b3f5
Ivan Murashchenko <imurashchenko@gmail.com>
2022-12-14 18:24:32 +0300

    feat(hub): dapps card

--- src/discover/components/dappsCard/DappsCard.tsx
+++ src/discover/components/dappsCard/DappsCard.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { push } from 'connected-react-router';
+import {
+  CloseIcon,
+  ChevronLeftIcon,
+  ChevronDown,
+  ChevronUp,
+} from 'common/icons';
+import { IconButton } from 'common';
 import { connect, ConnectedProps } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { RootState } from 'application/store';
+import { Collapse } from '@mui/material';
 import { getDappsCardData } from 'discover/selectors/discoverSelectors';
 import { RoutesEnum } from 'application/typings/routes';
 import styles from './DappsCard.module.scss';
@@ -20,19 +28,80 @@ const mapDispatchToProps = {
 const connector = connect(mapStateToProps, mapDispatchToProps);
 type DappsCardProps = ConnectedProps<typeof connector> & OwnProps;
 
-class DappsCardComponent extends React.PureComponent<DappsCardProps, never> {
+type DappsCardState = {
+  showMoreInfo: boolean;
+};
+
+class DappsCardComponent extends React.PureComponent<DappsCardProps, DappsCardState> {
+  constructor(props: DappsCardProps) {
+    super(props);
+
+    this.state = {
+      showMoreInfo: false,
+    };
+  }
+
+  toggleShowMore = () => {
+    this.setState((state) => ({ showMoreInfo: !state.showMoreInfo }));
+  };
+
   handleRouteToDappsList = () => {
     const { routeTo } = this.props;
 
     routeTo(RoutesEnum.discover);
   };
 
+  renderGenre = (genre: string) => <div
+    key={genre}
+    className={styles.dappsCardGenre}
+  >
+    {genre}
+  </div>;
+
+  renderShowMoreButton = () => {
+    const { showMoreInfo } = this.state;
+    return <div className={styles.dappsCardShowMoreButton} onClick={this.toggleShowMore}>
+      <span className={styles.dappsCardShowMoreButtonLabel}>
+        {showMoreInfo ? 'Show less' : 'Show more'}
+      </span>
+      {showMoreInfo ? <ChevronUp /> : <ChevronDown />}
+    </div>;
+  };
+
   render() {
     const { currentDappsData } = this.props;
-    console.log(this.props.currentDappsData);
-    return <div className={styles.dappsCard} onClick={this.handleRouteToDappsList}>
-      <div className={styles.dappsCoverHolder}>
-        <img src={currentDappsData?.card?.cover} alt={'dappsCover'} />
+    const { showMoreInfo } = this.state;
+
+    return <div className={styles.dappsCard}>
+      <div className={styles.dappsCardControls}>
+        <IconButton onClick={this.handleRouteToDappsList}>
+          <ChevronLeftIcon />
+        </IconButton>
+        <IconButton onClick={this.handleRouteToDappsList}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <div className={styles.dappsCoverHolder} style={{ backgroundImage: `url(${currentDappsData?.card?.cover})` }}>
+        <div className={styles.dappsCardGenreHolder}>
+          {currentDappsData?.card.genre.map(this.renderGenre)}
+        </div>
+        <div className={styles.dappsCardSmallCover}>
+          <img src={currentDappsData?.card?.smallCover} alt={'dappsSmallCover'} />
+        </div>
+      </div>
+      <div className={styles.dappsCardInfo}>
+        <div className={styles.dappsCardTitle}>
+          {currentDappsData?.card.title}
+        </div>
+        <div className={styles.dappsCardDesc}>
+          {currentDappsData?.card.description}
+        </div>
+        <Collapse in={showMoreInfo}>
+          <div className={styles.dappsCardDesc}>
+            {currentDappsData?.card.fullDescription}
+          </div>
+        </Collapse>
+        {this.renderShowMoreButton()}
       </div>
     </div>;
   }
