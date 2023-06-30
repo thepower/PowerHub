6a09644c38e8d5e1b18b0bfac99077da197f029e
jackkru69 <www.jackkru69@mail.ru>
2023-06-30 12:01:07 +0300

    fix appConstants

--- src/appConstants.ts
+++ src/appConstants.ts
@@ -1,5 +1,6 @@
-const isDev = process.env.NODE_ENV === 'development';
+// const isDev = process.env.NODE_ENV === 'development';
 export const explorerThePowerUrl = 'https://explorer.thepower.io';
-export const walletThePowerUrl = isDev ? 'http://localhost:3001' : 'https://wallet.thepower.io';
+// export const walletThePowerUrl = isDev ? 'http://localhost:3001' : 'https://test.wallet.thepower.io';
+export const walletThePowerUrl = 'https://test.wallet.thepower.io';
 export const hubThePowerUrl = 'https://hub.thepower.io';
 export const faucetThePowerUrl = 'https://faucet.thepower.io';
